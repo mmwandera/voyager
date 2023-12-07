828322811bdb9378369fdb691e59e4e03461674c
mmwandera <mmwandera74@gmail.com>
2023-12-07 14:34:52 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Voyager
+      Voyager: Coming soon
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
